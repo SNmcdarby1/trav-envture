e1aeeb4b9c881cf824d92cc99931127f6c429edb
SNmcdarby1 <sngereso@gmail.com>
2020-12-23 07:53:37 +0000

    add navbar,logo,images

---
+++ script.js
@@ -0,0 +1,177 @@
+// Sidenav
+var e = element();
+const icons = document.querySelector('.sidenav');
+M.SideNav.init(SideNav, {
+    menu
+});
+
+const icons = document.querySelector('.nav-item');
+M.Nav - item.init(Nav - item, {
+    menu
+});
+
+const icons = document.querySelectorByClassName('toggle-button')[0]
+const navbarLinks = document.querySelectorByClassName('nav-links active')[0]
+
+toggleButton.addEventListener('click'.anchor() === {
+    navlinks()
+});
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function() {
+    $('.carousel').carousel();
+});
+$('#myTab a').on('click', function() {
+
+    VarDate.apply(Function)
+    $(active).preventDefault()
+    $(this).tab('show')
+    $('#myTab a[href="#index"]').tab('show') // Select tab by name
+    $('#myTab li:first-child a').tab('show') // Select first tab
+    $('#myTab li:last-child a').tab('show') // Select last tab
+    $('#myTab li:nth-child(3) a').tab('show') // Select third tab
+});
+
+
+const ac = document.querySelector('.autocomplete');
+M.Autocomplete.init(ac, {
+    data: {
+        "Abi Dhabi": null,
+        "Rio": null,
+        "Spain": null,
+        "Aruba": null,
+        "Kenya": null,
+        "London": null,
+        "Dublin": null,
+        "Cancun Mexico": null,
+        "Hawai": null,
+        "Florida": null,
+        "New York": null,
+        "Jamaica": null,
+        "France": null,
+        "Australia": null,
+        "China": null,
+        "Netherlands": null,
+        "China": null,
+        "Malaysia": null,
+        "Newzealand": null,
+        "Ontrio": null,
+        "Auckland": null,
+
+    }
+});
+// Material Boxed
+const mb = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(mb, {});
+
+// Scrollspy
+const ss = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(ss, {});
+
+const ac = document.querySelector('.a.mobie-nav');
+M.Autocomplete.init(ac, {
+    data: {
+        "Abi Dhabi": null,
+        "Rio": null,
+        "Spain": null,
+        "Aruba": null,
+        "Kenya": null,
+        "London": null,
+        "Dublin": null,
+        "Cancun Mexico": null,
+        "Hawai": null,
+        "Florida": null,
+        "New York": null,
+        "Jamaica": null,
+        "France": null,
+        "Australia": null,
+        "China": null,
+        "Netherlands": null,
+        "China": null,
+        "Malaysia": null,
+        "Newzealand": null,
+        "Ontrio": null,
+        "Auckland": null,
+
+    }
+});
+// Material Boxed
+$("button").click(function() {
+    $("button").toggleClass(".tab");
+});
+$(newFunction('')).css("background-color", "yellow");
+
+
+var countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", newFunction_1()];
+
+$('#myCarousel').on('slid.bs.carousel', function(_) {
+    $('#myCarousel').carousel('2') // Will slide to the slide 2 as soon as the transition to slide 1 is finished
+});
+$(document).ready(function() {
+    $("p").click(function() {
+        $(this).hide();
+    });
+});
+$('#myCarousel').carousel('2') // Will slide to the slide 2 as soon as the transition to slide 1 is finished
+$(document).ready(function() {
+    $("p").click(function() {
+        $(this).hide();
+    });
+});
+$(function() {
+    $('#myTab li:last-child a').tab('show')
+});
+card - columns {
+    @include media - breakpoint - only(lg) {
+        column - count: 4;
+    }
+    @include media - breakpoint - only(xl) {
+        column - count: 5;
+    }
+}
+
+function newFunction_1() {
+    return "Bulgaria";
+}
+
+function newFunction() {
+    $grid - columns;
+    12;
+    $grid - gutter - width;
+    30;
+    px;
+    $grid - breakpoints;
+    (xs: 0, _sm: 540, px, {
+        return: ,
+        md: ,
+        768: px,
+    }, {
+        return: ,
+        lg: ,
+        992: px,
+    }, {}) => ;
+    ();
+    return;
+    any;
+    xl: 1200;
+    px; {
+        return;
+        $container - max - widths;
+        () => {
+            540;
+            px,
+            720;
+            px,
+            960;
+            px,
+            1140;
+            px;
+        };
+        return;
+    };
+} <
